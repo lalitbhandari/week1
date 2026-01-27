7df0b1b574427c46ef44be3db833db768f995540
lalit Bhandari <bhandarilalit171@gmail.com>
2026-01-27 21:37:01 +0545

    While condition

---
+++ week1/condition_While.java
@@ -0,0 +1,22 @@
+package week1;
+
+
+/**
+ * Write a description of class condition_While here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class condition_While
+{
+    public static void main (String [] args)
+    {
+        int count= 0;
+        while(count<6)
+        {
+            System.out.println(""+count);
+            count++;
+        }
+        
+    }
+}
---
+++ week1/whileCondition.java
@@ -0,0 +1,21 @@
+package week1;
+
+
+/**
+ * Write a description of class whileCondition here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class whileCondition
+{
+    public static void main(String [] args)
+    {
+        int count=0;
+        while(count <=8)
+        {
+            System.out.println(""+count);
+            count++;
+        }
+    }
+}
