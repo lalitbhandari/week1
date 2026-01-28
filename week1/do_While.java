63e928444b444ec23e9ebfff652f3bdd000ed740
lalit Bhandari <bhandarilalit171@gmail.com>
2026-01-28 20:35:18 +0545

    do while loop

---
+++ week1/do_While.java
@@ -0,0 +1,21 @@
+package week1;
+
+
+/**
+ * Write a description of class do_While here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class do_While
+{
+    public static void main (String [] args)
+    {
+        int a=0;
+        do{
+             a++;
+             System.out.println(a); 
+        }
+         while(a<6);
+    }
+}
