e63ed33bc5ab3b39f757acd0d6d689ee08f470ed
lalit Bhandari <bhandarilalit171@gmail.com>
2026-01-29 21:13:29 +0545

    difreence

---
+++ week1/diferrence.java
@@ -0,0 +1,24 @@
+package week1;
+
+
+/**
+ * Write a description of class diferrence here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class diferrence
+{
+    public static void main(String [] args)
+    {
+        for( int count =100; count>= 0; count --)
+        {
+            System.out.println(count);
+        }
+        
+        
+        
+        
+        
+    }
+}
