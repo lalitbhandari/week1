a059310970ab2823b81132fc93be839e80d9e03a
lalit Bhandari <bhandarilalit171@gmail.com>
2026-01-31 19:45:05 +0545

    square natural num

---
+++ week1/square_Natural_Num.java
@@ -0,0 +1,19 @@
+package week1;
+
+
+/**
+ * Write a description of class square_Natural_Num here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class square_Natural_Num
+{
+    public static void main (String [] args)
+    {
+        for(int square =1; square<=10; square++)
+        {
+          System.out.println(square*square);  
+        }
+    }
+}
