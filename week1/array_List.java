7332e6509d81dbd2a18eaec5456e1f012c17e517
lalit Bhandari <bhandarilalit171@gmail.com>
2026-02-01 20:11:45 +0545

    week 10

---
+++ week1/array_List.java
@@ -0,0 +1,21 @@
+package week1;
+
+
+/**
+ * Write a description of class array_List here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class array_List
+{
+    public static void main(String [] args)
+    {
+        int [] a={1,4,54,6,56,54};
+            for (int i=0; i<a.length; i++)
+            {
+            System.out.println(a[i]); 
+        }
+       
+    }
+}
---
+++ week1/display_Pattern.java
@@ -0,0 +1,19 @@
+package week1;
+
+
+/**
+ * Write a description of class display_Pattern here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class display_Pattern
+{
+    public static void main(String [] args)
+    {
+     for(int i=0; i<6; i++)
+     {
+         System.out.println(i);
+     }
+}
+  }
