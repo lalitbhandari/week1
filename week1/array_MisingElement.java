d4554744bd1538a55eb15e17544690706e930276
lalit Bhandari <bhandarilalit171@gmail.com>
2026-02-02 20:25:52 +0545

    misiing element

---
+++ week1/array_MisingElement.java
@@ -0,0 +1,22 @@
+package week1;
+
+
+/**
+ * Write a description of class array_MisingElement here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class array_MisingElement
+{
+    public static void main(String [] args)
+    {
+        int[]arr={1,2,3,5};
+        int sum;
+        for (int i =0; i<=5; i++)
+        {
+            sum +=i;
+        }
+        
+    }
+}
